9ac406b865475b17c6a5d94fc52d68510f7d1bc0
Lucas <lucassouza.zip@gmail.com>
2023-11-10 20:45:20 -0300

    add routes to CRUD

--- router/router.go
+++ router/router.go
@@ -3,13 +3,11 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
-	//Declaração efemera
+	// Initialize Router
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	initializeRoutes(r)
+
+	// Run the server
 	r.Run(":8080")
 }
