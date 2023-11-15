6f311a6bc31ab043d18fc8d939e8d2fef35e299f
Lucas <lucassouza.zip@gmail.com>
2023-11-15 16:52:33 -0300

    add custom logger and routes

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/lucassouzz/gopportunities/router"
+import (
+	"github.com/lucassouzz/gopportunities/config"
+	"github.com/lucassouzz/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		panic(err)
+	}
+
 	router.Initialize()
 }
